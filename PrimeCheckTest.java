b9ea7bdd2b6c899abd387b6845ddc05023f273ee
mothman2503 <othmanghani.13@gmail.com>
2023-06-30 14:52:16 +0200

    Schritt 3(2)

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -24,7 +24,7 @@ public class PrimeCheckTest {
   public void testPrimes() {
     int[] arr = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
     for (int i = 0; i < arr.length; ++i) {
-      assertEquals("Test negative number " + arr[i], false, PrimeCheck.isPrime(arr[i]));
+      assertEquals("Test negative number " + arr[i], true, PrimeCheck.isPrime(arr[i]));
     }
   }
 
